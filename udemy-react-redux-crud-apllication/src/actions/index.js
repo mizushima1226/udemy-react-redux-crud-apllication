9beadbe0285c918d5c84d0a818569c474f28616c
mizushima1226 <mizuki.bz.3224@gmail.com>
2019-11-28 07:27:54 +0900

    implement action and action creator

---
+++ udemy-react-redux-crud-apllication/src/actions/index.js
@@ -0,0 +1,10 @@
+export const INCREMENT = "INCRIMENT";
+export const DECRIMENT = "DECRIMENT";
+
+export const incriment = () => ({
+    type: INCRIMENT
+})
+
+export const decriment = ()=> ({
+    type: DECRIMENT
+})
