4624894f097a551c2d74e8eb0ef04289f26de0cb
mizushima1226 <mizuki.bz.3224@gmail.com>
2019-11-28 21:26:03 +0900

    implement events index

--- udemy-react-redux-crud-apllication/src/actions/index.js
+++ udemy-react-redux-crud-apllication/src/actions/index.js
@@ -1,10 +1,11 @@
-export const INCRIMENT = "INCRIMENT";
-export const DECRIMENT = "DECRIMENT";
+import axios from 'axios';
 
-export const incriment = () => ({
-    type: INCRIMENT
-})
+export const READ_EVENTS = 'READ_EVENTS';
 
-export const decriment = ()=> ({
-    type: DECRIMENT
-})
+const ROOT_URL = 'https://udemy-utils.herokuapp.com/api/v1';
+const QUERYSTRING = '?token=token123';
+
+export const readEvents = () => async dispatch => {
+    const response = await axios.get(`${ROOT_URL}/events${QUERYSTRING}`)
+    dispatch({type: READ_EVENTS, response});
+};
